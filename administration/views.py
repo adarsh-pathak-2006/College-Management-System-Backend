d5f68ad9bcf48c3718109062c6775eb77f61b55f
duckyduck01 <madhvetipathak2006@gmail.com>
2026-06-27 22:09:59 +0530

    making views for courses subjects etc

--- administration/views.py
+++ administration/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.views import View
 from administration.forms import RegisterForm
+from rest_framework.views import APIView
+from administration.serializers import DepartmentSerializer, CourseSerializer, SubjectSerializer, NoticeSerializer, UserSerializer
+from administration.models import Department, Course, Subject, Notice
+from rest_framework.response import Response
 
 
 class RegisterView(View):
@@ -10,7 +14,7 @@ class RegisterView(View):
         return render(request, 'register.html', { 'form':form })
     
     def post(self, request):
-        form_input=RegisterForm(data=request.data)
+        form_input=RegisterForm(request.POST)
         if form_input.is_valid():
             fname=form_input.cleaned_data['first_name']
             lname=form_input.cleaned_data['last_name']
@@ -29,5 +33,69 @@ class RegisterView(View):
                     return redirect('register')
 
 
+class DepartmentAPI(APIView):
+    def get(self, request):
+        data=Department.objects.all()
+        serial=DepartmentSerializer(data, many=True)
+        return Response(serial.data)
+    
+    def post(self, request):
+        serial=DepartmentSerializer(data=request.data)
+        if serial.is_valid():
+            serial.save()
+            return Response(serial.data)
+        else:
+            return Response({ 'message':'invalid input' })
 
 
+class DepartmentAPI_individual(APIView):
+    def get(self, request, pk):
+        data=get_object_or_404(Department, id=pk)
+        serial=DepartmentSerializer(data)
+        return Response(serial.data)
+    
+    def put(self, request, pk):
+        instance=get_object_or_404(Department, id=pk)
+        serial=DepartmentSerializer(instance, data=request.data)
+        if serial.is_valid():
+            serial.save()
+            return Response(serial.data)
+        else:
+            return Response({ 'message':'invalid input' })
+    
+    def delete(self, request, pk):
+        data=get_object_or_404(Department, id=pk)
+        data.delete()
+        return Response({"message": "Deleted successfully"})
+
+
+
+class CourseAPI(APIView):
+    def get(self, request):
+        data=Course.objects.all()
+        serial=CourseSerializer(data, many=True)
+        return Response(serial.data)
+
+    def post(self, request):
+        serial=CourseSerializer(data=request.data)
+        if serial.is_valid():
+            serial.save()
+            return Response(serial.data)
+        else:
+            return Response({ 'message':'invalid input' })
+        
+class CourseAPI_individual(APIView):
+    def get(self, request, pk):
+        instance=get_object_or_404(Course, id=pk)
+        serial=CourseSerializer(instance)
+        return Response(serial.data)
+    
+    def put(self, request, pk):
+        instance=get_object_or_404(Course, id=pk)
+        serial=CourseSerializer(instance, data=request.data)
+        if serial.is_valid():
+            serial.save()
+            return Response(serial.data)
+        
+        else:
+            return Response({ 'message':'invalid input' })
